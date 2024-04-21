ad859eb2f11e560da563fb78ffec2311f0e99599
Uladzislau, Danik (contracted) <Danik.Uladzislau@contracted.pmi.com>
2024-04-21 13:36:13 +0200

    little improvements

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -4,7 +4,7 @@ function AboutMe({ data }) {
     return (
         <div id='aboutMe' className='secition'>
             <p>{data}</p>
-            <img src='https://avatars.githubusercontent.com/u/77941004?v=4' alt='MyPicture'></img>
+            <img src='photo_5431819627601709746_y.jpg' alt='MyPicture'></img>
         </div>
     );
 }
